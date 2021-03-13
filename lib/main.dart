74f4549744ca7594362338bf0d4119a940dd715d
snt-nbzw <sntnbzw@gmail.com>
2021-03-14 00:12:21 +0900

    Add: create the menu without animations

--- lib/main.dart
+++ lib/main.dart
@@ -14,22 +14,83 @@ class ExampleStaggeredAnimationsPage extends StatefulWidget {
 
 class _ExampleStaggeredAnimationsPageState
     extends State<ExampleStaggeredAnimationsPage> {
+  static const _menuTitles = [
+    'Declarative Style',
+    'Premade Widgets',
+    'Stateful Hot Reload',
+    'Native Performance',
+    'Great Community',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          _buildFlutterLogo(),
+          _buildContent(),
+        ],
+      ),
     );
   }
-}
 
-class Menu extends StatefulWidget {
-  @override
-  _MenuState createState() => _MenuState();
-}
-
-class _MenuState extends State<Menu> {
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildFlutterLogo() {
     return Container();
+  }
+
+  Widget _buildContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 16),
+        ..._buildListItems(),
+        const Spacer(),
+        _buildGetStartedButton(),
+      ],
+    );
+  }
+
+  List<Widget> _buildListItems() {
+    final listItems = <Widget>[];
+    for (var i = 0; i < _menuTitles.length; ++i) {
+      listItems.add(
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 16),
+          child: Text(
+            _menuTitles[i],
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      );
+    }
+    return listItems;
+  }
+
+  Widget _buildGetStartedButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: RaisedButton(
+          shape: const StadiumBorder(),
+          color: Colors.blue,
+          padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 14),
+          onPressed: () {},
+          child: const Text(
+            'Get Started',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
